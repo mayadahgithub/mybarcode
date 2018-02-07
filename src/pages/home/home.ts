56922d98901f794b2c94b9ce11bdc3a2ba195997
mayadahgithub <mayadahnaji@gmail.com>
2018-02-07 15:01:41 +0300

    add barcode

---
+++ src/pages/home/home.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner,BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+data={ };
+encodemyData:string;
+encodedData:{};
+
+ option:BarcodeScannerOptions;
+  constructor(public navCtrl: NavController,
+    public barcodeScanner:BarcodeScanner ) {
+
+  }
+
+scan(){
+  this.option ={
+  
+    prompt: "please scan your code" 
+  }
+  this.barcodeScanner.scan(this.option).then((barcodeData) => {
+    // Success! Barcode data is here
+    console.log(barcodeData);
+    this.data = barcodeData;
+    
+   }, (err) => {
+       // An error occurred
+       console.log(err);
+   }); 
+
+}
+
+encodeData(){
+this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,this.encodemyData)
+.then((res)=>{
+  console.log(res)
+  this.encodeData = res; 
+}, (err) => {
+  // An error occurred
+  console.log(err);
+
+})
+}
+
+
+
+}
